26706a9d0f3dd7286cd78cd2a9b71f93a11b9abd
周兴邦 <>
2021-05-25 15:17:31 +0800

    优化请求方式

---
+++ webui/src/utils/request.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const http = axios.create({
+  timeout: 60 * 5 * 1000
+})
+http.interceptors.request.use(
+  config => {
+    config.headers = {
+      'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoidGVzdCIsImV4cCI6MTYxMTUwMDM3NCwiaXNzIjoiSnd0S2V5TmFtZSIsImF1ZCI6Ikp3dEtleU5hbWUifQ.8nnKmig8Ar5uAC5WDZLxJZEl9mEpPsqbN4YrBt-H_UQ'
+    }
+    return config
+  }, error => {
+    console.log(error)
+    return Promise.reject(error)
+  }
+)
+
+http.interceptors.response.use(response => {
+  return response.data
+})
+
+export default http
---
+++ webui/src/views/lift_data_mgmt.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+import {getTest} from '../utils/api'
+
+export default {
+  name: `lift-data-mgmt`,
+  async created () {
+    const a = await getTest('cscb001')
+    console.log(a)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
